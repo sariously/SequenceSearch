137941358a232ee35ee336302c68e0d8f7e1da13
kazemicode <sarieses@gmail.com>
2018-03-21 10:52:23 -0700

    Set master

---
+++ src/main/java/edu/gcccd/csis/p1/SequenceSearchImpl.java
@@ -0,0 +1,30 @@
+package edu.gcccd.csis.p1;
+
+public class SequenceSearchImpl extends SequenceSearch {
+
+    public SequenceSearchImpl(final String content, final String start, final String end) {
+        super(content, start, end);
+    }
+
+    @Override
+    public String[] getAllTaggedSequences() {
+      return null;
+    }
+
+    @Override
+    public String getLongestTaggedSequence() {
+
+        return null;
+    }
+
+    @Override
+    public String displayStringArray() {
+       return null;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+}
